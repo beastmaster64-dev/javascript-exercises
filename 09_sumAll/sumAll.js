b770c3ed1eab6118568a2b03bc12bc86d7cd3797
BeastMaster 64 <244566067+beastmaster64-dev@users.noreply.github.com>
2025-12-16 14:48:18 +0530

    Solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+
+    //check if the inputs are integers
+
+    if(Number.isInteger(number1) == false || Number.isInteger(number2) == false){
+        return "ERROR";
+    }
+
+    //check if the inputs are non-negative
+
+    if(number1 < 0 || number2 < 0){
+        return "ERROR";
+    }
+
+    //check if number 1 is greater than number 2
+    if(number1 > number2){
+        let temp = number1;
+        number1 = number2;
+        number2 = temp;
+    }
+
+    let sum = 0;
+    for(let i=number1; i<=number2;i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
