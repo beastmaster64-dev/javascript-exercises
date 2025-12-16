200d0f8e7965d667bfce91d26440ad0b93f51d2a
BeastMaster 64 <244566067+beastmaster64-dev@users.noreply.github.com>
2025-12-16 13:09:03 +0530

    Solve 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(input_str, num_repeat) {
+
+    if(num_repeat<0){
+        return "ERROR";
+    }
+
+    if(num_repeat == 0){
+        return '';
+    }
+
+    let output_str = input_str;
+    for(let i=2;i<=num_repeat;i++){
+        output_str += input_str;
+    }
+
+    return output_str;
 
 };
 
