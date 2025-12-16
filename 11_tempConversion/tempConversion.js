466f13dd838c7f960e5752e41161af388d66d44e
BeastMaster 64 <244566067+beastmaster64-dev@users.noreply.github.com>
2025-12-16 15:17:00 +0530

    Solve 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemperature) {
+
+  let outputTemperature = (inputTemperature - 32)*(5/9);
+  
+  return Math.round(outputTemperature*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemperature) {
+
+  let outputTemperature = inputTemperature*(9/5) + 32;
+
+  return Math.round(outputTemperature*10)/10;
+
+
 };
 
 // Do not edit below this line
